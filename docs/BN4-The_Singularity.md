0b139b56632a82015a862ccb2aa86bd3ff5a8367
Adam Adair <adamadair@outlook.com>
2022-04-05 23:58:06 -0500

    fixed infinity-attack script

---
+++ docs/BN4-The_Singularity.md
@@ -0,0 +1,30 @@
+# HOWTO complete BN4 
+
+Most methods of making money are completely nerfed in this bitnode. You will not get
+far if you try to rely on hacking or the hacknet to make cash, but this is obvious
+as soon as you get into it. As with the other bitnodes, they are trying to steer you
+towards a different way to solve the same problem, but in this case it might not be 
+obvious as they leave no clues at all as to the best way to get started. After poking around
+a bit I figured out a path to completion and finished it in a little over 2 days. I think
+I could do it faster a second time now that I know what I am doing. Here is the basic plan:
+
+1. Level up hacking enough to run infinity-remote-spinup.js to get the easy servers making money. You need $11m.
+
+2. Get the first RAM upgrade on your home computer and then run grow-bad-karma.js. You need to get to 18K kills, and 
+this will take a while, about 15 hours or so. This is really boring, but this is using the singularity 
+functions as advertised. Once you grow sufficient bad karma you can start a gang.
+
+3. Create your gang with the slum snakes, or any other combat gang.
+
+4. run manage-combat-gang.js, and let it level up your gang until you get 100%
+
+5. Once you get $31b, buy everything you need for stock market trading. Run be-the-wolf.js.
+
+6. Once you get $150b start your corpporation, and follow the bn3 notes to make a shit ton of money.
+
+7. Once you have a steady gang and corporation income, buy every hacking related augmentation from your gang and 
+finish the bitnode. Don't be afraid to install augmentations often, your gang and corp will make it up quickly. 
+I think I installed 5 or 6 times before completing the bitnode
+
+
+
--- scripts/infinity-attack.js
+++ scripts/infinity-attack.js
@@ -1,6 +1,13 @@
 /** @type import(".").NS */
 let ns = null;
-
+const HACK_SCRIPT = "infinity-hack.js";
+const GROW_SCRIPT = "infinity-grow.js";
+const WEAKEN_SCRIPT = "infinity-weaken.js";
+let canBruteSsh = false;
+let canFtpCrack = false;
+let canRelaySmtp = false;
+let canHttpWorm = false;
+let canSqlInject = false;
 /** @param {NS} _ns **/
 export async function main(_ns) {
     ns = _ns;
@@ -8,50 +15,147 @@ export async function main(_ns) {
     const data = ns.flags([
         ["target", ""],  // --target => the target (hostname) of the attack
         ["threads", 0],  // --threads => number of threads to use
-        ["max", false],   // --max => use max available threads
-        ["id", 0]
+        ["max", false],  // --max => use max available threads
+        ["id", 0]        // --id => process id you use, useful for killing the right process.
     ]);
 
-    let threads = data.threads;
-    if(data.max){
-        let scriptMemoryRequired = ns.getScriptRam(scriptName);
-        let availableRam = ns.getServerMaxRam("home") - ns.getServerUsedRam("home");
-        threads = parseInt(availableRam / scriptMemoryRequired);
-    }
+    // Make sure arguments make sense.
     if (data.target == "") {
         ns.tprint("No target provided, exiting now.")
         return;
     }
+    if(data.threads > 0 && data.max){
+        ns.print("You can't request max threads then specify thread count. Use either --threads or --max, not both");
+        return;
+    }
 
     let server = data.target;
+    // Check hack level
+    const myHackLevel = ns.getHackingLevel();
+    const requiredHackLevel = ns.getServerRequiredHackingLevel(server);
+    if (myHackLevel < ns.getServerRequiredHackingLevel(server)) {
+        ns.tprintf("ERROR: %s requires hack level %d, you only have %d", server, requiredHackLevel, myHackLevel);
+        ns.exit();
+        return;
+    }
+    let node = ns.getServer(server);
+    if (!node.hasAdminRights) {
+        // Need admin rights, can Nuke?
+        let portAttackCount = checkForLocalPrograms();
+        if (portAttackCount < node.numOpenPortsRequired) {
+            nukeHost(node.hostname);
+        } else {
+            ns.tprintf("ERROR: %s requires %d ports open to nuke, you can only open %d", node.hostname, node.numOpenPortsRequired, portAttackCount);
+            ns.exit();
+            return;
+        }
+    }
+
+    // There must be atlease 1 of each type of operation (hack, grow, weaken)
+    // If there is not enough memory for 3 threads, we error out.
+    let scriptMemoryRequired = ns.getScriptRam(scriptName);
+    let availableRam = ns.getServerMaxRam("home") - ns.getServerUsedRam("home");
+    const threadsAvailable = parseInt(availableRam / scriptMemoryRequired);
+    if(threadsAvailable < 3){
+        ns.tprintf("ERROR: Not enough memory availabl to attack %s", server);
+        ns.exit();
+        return;
+    }    
+
+
     let hackThreads = ns.hackAnalyze(server);
     if (hackThreads > 0) {
         hackThreads = parseInt(.10 / hackThreads) + 1; // 10% of full hack
     } else {
         hackThreads = 1;
     }
+
     let growThreads = parseInt(ns.growthAnalyze(server, 4)) + 1;
+    if (isNaN(growThreads) || growThreads < 1) {
+        growThreads = 1;
+    }    
     let weakenThreads = hackThreads * 2;
-    if (threads > 0) {
+    
+
+    let threads = data.threads;    
+    if(data.max || threads > threadsAvailable){
+        threads = threadsAvailable;
+    }
+
+    if (threads > 0 || hackThreads + growThreads + weakenThreads > threadsAvailable) {
+        if(threads == 0) {
+            threads = threadsAvailable;
+        }
         let total = hackThreads + growThreads + weakenThreads;
         hackThreads = parseInt((hackThreads / total) * threads);
-        if(hackThreads<1){
+        if (hackThreads < 1) {
             hackThreads = 1;
         }
         growThreads = parseInt((growThreads / total) * threads);
+        if (isNaN(growThreads) || growThreads < 1) {
+            growThreads = 1;
+        }    
         weakenThreads = threads - (hackThreads + growThreads);
-        if(weakenThreads < 1){
+        if (weakenThreads < 1) {
             weakenThreads = 1;
             growThreads--;
         }
-    }    
-    
+    }
+
     let id = data.id;
-    if(id==0){
+    if (id == 0) {
         id = Date.now();
     }
     ns.tprintf("Attacking %s with %d hack; %d grow; %d weaken threads.", server, hackThreads, growThreads, weakenThreads);
     ns.exec("infinity-hack.js", "home", hackThreads, "--target", server, "--id", id);
     ns.exec("infinity-grow.js", "home", growThreads, "--target", server, "--id", id);
     ns.exec("infinity-weaken.js", "home", weakenThreads, "--target", server, "--id", id);
+}
+
+// nuke the host for root access
+function nukeHost(hostname) {
+    if (canBruteSsh)
+        ns.brutessh(hostname);
+    if (canFtpCrack)
+        ns.ftpcrack(hostname);
+    if (canRelaySmtp)
+        ns.relaysmtp(hostname);
+    if (canHttpWorm)
+        ns.httpworm(hostname);
+    if (canSqlInject)
+        ns.sqlinject(hostname);
+    ns.nuke(hostname);
+    ns.tprintf("%s nuked!", hostname);
+}
+
+// check for installed hacking programs. 
+function checkForLocalPrograms() {
+    let portAttackCount = 0;
+    if (ns.fileExists("BruteSSH.exe", "home")) {
+        portAttackCount++;
+        canBruteSsh = true;
+    }
+    if (ns.fileExists("FTPCrack.exe", "home")) {
+        portAttackCount++;
+        canFtpCrack = true;
+    }
+    if (ns.fileExists("relaySMTP.exe", "home")) {
+        portAttackCount++;
+        canRelaySmtp = true;
+    }
+    if (ns.fileExists("HTTPWorm.exe", "home")) {
+        portAttackCount++;
+        canHttpWorm = true;
+    }
+    if (ns.fileExists("SQLInject.exe", "home")) {
+        portAttackCount++;
+        canSqlInject = true;
+    }
+    ns.printf("Attacks: %d", portAttackCount);
+    ns.print("canBruteSsh: " + canBruteSsh);
+    ns.print("canFtpCrack: " + canFtpCrack);
+    ns.print("canRelaySmtp: " + canRelaySmtp);
+    ns.print("canHttpWorm: " + canHttpWorm);
+    ns.print("canSqlInject: " + canSqlInject);
+    return portAttackCount;
 }
