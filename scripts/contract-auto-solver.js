e12803f623c4a7ff2a679fcb9fa1d25280c68ae8
Adam Adair <adamadair@outlook.com>
2022-05-24 20:36:38 -0500

    Added compression II and III solutions to contract auto solver

--- scripts/contract-auto-solver.js
+++ scripts/contract-auto-solver.js
@@ -12,6 +12,8 @@
  *      Algorithmic Stock Trader IV
  *      Array Jumping Game
  *      Array Jumping Game II
+ *      Compression II: LZ Decompression
+ *      Compression III: LZ Compression
  *      Find All Valid Math Expressions
  *      Find Largest Prime Factor
  *      Generate IP Addresses
@@ -135,15 +137,21 @@ function solve(type, data, server, contract, ns) {
         case "Shortest Path in a Grid":
             solution = shortestPathInGrid(data);
             break;
-        case  "Proper 2-Coloring of a Graph":
+        case "Proper 2-Coloring of a Graph":
             solution = proper2ColoringOfAGraph(data);
+            break;
+        case "Compression III: LZ Compression":
+            solution = compressionIII(data);
+            break;
+        case "Compression II: LZ Decompression":
+            solution = decompressII(data);
             break;
         default:
             ns.tprintf("ERROR: Contract type '%s' has no solving function.", type);
             solution = "$FUCKMEINTHEGOATASS!"
             break;
     }
-    return ("$FUCKMEINTHEGOATASS!" != solution) ? ns.codingcontract.attempt(solution, contract, server, [true]) : false;
+    return ("$FUCKMEINTHEGOATASS!" !== solution) ? ns.codingcontract.attempt(solution, contract, server, [true]) : false;
 }
 
 //ALGORITHMIC STOCK TRADER
@@ -834,3 +842,138 @@ function proper2ColoringOfAGraph(data){
     return colors;
 }
 
+// Compression III
+
+function compressionIII(data) {
+    let cur_state = Array.from(Array(10), () => Array(10).fill(null));
+    let new_state = Array.from(Array(10), () => Array(10));
+    function set(state, i, j, str) {
+        const current = state[i][j];
+        if (current == null || str.length < current.length) {
+            state[i][j] = str;
+        }
+        else if (str.length === current.length && Math.random() < 0.5) {
+            state[i][j] = str;
+        }
+    }
+
+    cur_state[0][1] = "";
+    for (let i = 1; i < data.length; ++i) {
+        for (const row of new_state) {
+            row.fill(null);
+        }
+        const c = data[i];
+        for (let length = 1; length <= 9; ++length) {
+            const string = cur_state[0][length];
+            if (string == null) {
+                continue;
+            }
+            if (length < 9) {
+                set(new_state, 0, length + 1, string);
+            }
+            else {
+                set(new_state, 0, 1, string + "9" + data.substring(i - 9, i) + "0");
+            }
+            for (let offset = 1; offset <= Math.min(9, i); ++offset) {
+                if (data[i - offset] === c) {
+                    set(new_state, offset, 1, string + length + data.substring(i - length, i));
+                }
+            }
+        }
+
+        for (let offset = 1; offset <= 9; ++offset) {
+            for (let length = 1; length <= 9; ++length) {
+                const string = cur_state[offset][length];
+                if (string == null) {
+                    continue;
+                }
+                if (data[i - offset] === c) {
+                    if (length < 9) {
+                        set(new_state, offset, length + 1, string);
+                    }
+                    else {
+                        set(new_state, offset, 1, string + "9" + offset + "0");
+                    }
+                }
+
+                set(new_state, 0, 1, string + length + offset);
+                for (let new_offset = 1; new_offset <= Math.min(9, i); ++new_offset) {
+                    if (data[i - new_offset] === c) {
+                        set(new_state, new_offset, 1, string + length + offset + "0");
+                    }
+                }
+            }
+        }
+        const tmp_state = new_state;
+        new_state = cur_state;
+        cur_state = tmp_state;
+    }
+    let result = null;
+    for (let len = 1; len <= 9; ++len) {
+        let string = cur_state[0][len];
+        if (string == null) {
+            continue;
+        }
+        string += len + data.substring(data.length - len, data.length);
+        if (result == null || string.length < result.length) {
+            result = string;
+        }
+        else if (string.length === result.length && Math.random() < 0.5) {
+            result = string;
+        }
+    }
+    for (let offset = 1; offset <= 9; ++offset) {
+        for (let len = 1; len <= 9; ++len) {
+            let string = cur_state[offset][len];
+            if (string == null) {
+                continue;
+            }
+            string += len + "" + offset;
+            if (result == null || string.length < result.length) {
+                result = string;
+            }
+            else if (string.length === result.length && Math.random() < 0.5) {
+                result = string;
+            }
+        }
+    }
+    return result !== null && result !== void 0 ? result : "";
+}
+
+//Compression II: LZ Decompression
+
+function decompressII(data) {
+    let plain = "";
+    for (let i = 0; i < data.length;) {
+        const literal_length = data.charCodeAt(i) - 0x30;
+        if (literal_length < 0 || literal_length > 9 || i + 1 + literal_length > data.length) {
+            return null;
+        }
+        plain += data.substring(i + 1, i + 1 + literal_length);
+        i += 1 + literal_length;
+        if (i >= data.length) {
+            break;
+        }
+        const backref_length = data.charCodeAt(i) - 0x30;
+        if (backref_length < 0 || backref_length > 9) {
+            return null;
+        }
+        else if (backref_length === 0) {
+            ++i;
+        }
+        else {
+            if (i + 1 >= data.length) {
+                return null;
+            }
+            const backref_offset = data.charCodeAt(i + 1) - 0x30;
+            if ((backref_length > 0 && (backref_offset < 1 || backref_offset > 9)) || backref_offset > plain.length) {
+                return null;
+            }
+            for (let j = 0; j < backref_length; ++j) {
+                plain += plain[plain.length - backref_offset];
+            }
+            i += 2;
+        }
+    }
+    return plain;
+}
